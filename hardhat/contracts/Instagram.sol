25eb8a9854e83dd6ea92653fd63d8178e7d64951
Sebastian Nieto <snrnieto1999@hotmail.com>
2024-08-15 17:14:57 -0500

    [ADD] generating docs NatSpec

--- hardhat/contracts/Instagram.sol
+++ hardhat/contracts/Instagram.sol
@@ -2,45 +2,80 @@
 
 pragma solidity ^0.8.24;
 
+/**
+ @title Clone instagram Web3
+ @author Sebastian Nieto
+ @notice You can use this contract to emulate descentralized web3 social app
+ @dev A este contrato le faltan mas funcionalidades pero cumple con lo minimo
+ @custom:clonando este es un contrato minimalista de instagram 
+  */
 contract Instagram {
+    /// @notice Counter for posts
     uint256 public s_postCounterId;
-    // crear posts
-    // dar like a un post
-    // quitar like a un post
-    // tenemos un feed de posts
 
+    /**
+    @dev Un struct que define o representa un post en la plataforma
+    @param description la descripcion del post
+    @param uri la uri del post en IPFS
+     */
     struct Post {
         string description;
         string uri; // metadata
     }
 
+    /// @notice Mapping para almacenar los posts y su identificador
     mapping(uint256 => Post) private s_posts;
-    // Posts del usuario
+
+    /// @notice Mapping para almacenar los posts de cada usuario
     mapping(address => mapping(uint256 => Post)) private s_postsUser;
-    // Conteo de posts por usuario
-    mapping(address => uint256) private s_postCounterByUser;
-    // Like de las publicaciones segun postId
+
+    /// @notice Conteo de posts por usuario
+    mapping(address => uint256) private s_postsCounterByUser;
+
+    /// @notice Likes en las publicaciones según PostId
     mapping(uint256 => address[]) private s_likesOfPost;
 
-    event PostAdded(uint256 indexed _postId, string description, address owner);
-    event Like(uint256 indexed _postId, address user);
-    event Unlike(uint256 indexed _postId, address user);
-    error PostDoesNotExist(uint256);
+    /// @notice Evento emitido cuando se añade un nuevo post
+    /// @param postId El identificador del post
+    /// @param description La descripción del post
+    /// @param owner La dirección del propietario del post
+    event PostAdded(uint256 indexed postId, string description, address owner);
 
+    /// @notice Evento emitido cuando un usuario da like a un post
+    /// @param postId El identificador del post
+    /// @param user La dirección del usuario que dio like
+    event Like(uint256 indexed postId, address user);
+
+    /// @notice Evento emitido cuando un usuario quita su like de un post
+    /// @param postId El identificador del post
+    /// @param user La dirección del usuario que quitó su like
+    event Unlike(uint256 indexed postId, address user);
+
+    /// @notice Error lanzado cuando un post no existe
+    /// @param postId El identificador del post que no existe
+    error PostDoesNotExist(uint256 postId);
+
+    /// @notice Añade un nuevo post
+    /// @param _post La estructura del post que contiene la descripción y el URI
     function addPost(Post memory _post) external {
         require(bytes(_post.uri).length > 0, "Uri can't be empty");
         s_postCounterId++;
         s_posts[s_postCounterId] = _post;
-        s_postCounterByUser[msg.sender]++;
         s_postsUser[msg.sender][s_postCounterId] = _post;
+        s_postsCounterByUser[msg.sender]++;
         emit PostAdded(s_postCounterId, _post.description, msg.sender);
     }
 
-    function like(uint256 _postId) external postExists(_postId) {
+    /// @notice Da like a un post
+    /// @param _postId El identificador del post
+    function like(uint256 _postId) external postsExists(_postId) {
         s_likesOfPost[_postId].push(msg.sender);
         emit Like(_postId, msg.sender);
     }
-    function unlike(uint256 _postId) external postExists(_postId) {
+
+    /// @notice Quita el like de un post
+    /// @param _postId El identificador del post
+    function unlike(uint256 _postId) external postsExists(_postId) {
         address[] storage addresses = s_likesOfPost[_postId];
 
         for (uint256 index = 0; index < addresses.length; index++) {
@@ -51,60 +86,77 @@ contract Instagram {
                 break;
             }
         }
-
         emit Unlike(_postId, msg.sender);
     }
 
+    /// @notice Obtiene un post por su identificador
+    /// @param _postId El identificador del post
+    /// @return La estructura del post
     function getPost(
         uint256 _postId
-    ) public view postExists(_postId) returns (Post memory) {
+    ) public view postsExists(_postId) returns (Post memory) {
         return s_posts[_postId];
     }
 
+    /// @notice Obtiene un post de un usuario por su identificador
+    /// @param _user La dirección del usuario
+    /// @param _postId El identificador del post
+    /// @return La estructura del post
     function getPostUser(
         address _user,
         uint256 _postId
-    ) public view postExists(_postId) returns (Post memory) {
+    ) public view postsExists(_postId) returns (Post memory) {
         return s_postsUser[_user][_postId];
     }
 
+    /// @notice Obtiene el conteo de posts de un usuario
+    /// @param _user La dirección del usuario
+    /// @return El número de posts del usuario
     function getPostsCounterByUser(
         address _user
     ) public view returns (uint256) {
-        return s_postCounterByUser[_user];
+        return s_postsCounterByUser[_user];
     }
 
+    /// @notice Obtiene el número de likes de un post
+    /// @param _postId El identificador del post
+    /// @return El número de likes del post
     function getLikesOfPost(
         uint256 _postId
-    ) public view postExists(_postId) returns (uint256) {
+    ) public view postsExists(_postId) returns (uint256) {
         return s_likesOfPost[_postId].length;
     }
 
+    /// @notice Obtiene el URI de un post
+    /// @param _postId El identificador del post
+    /// @return El URI del post
     function getUri(
         uint256 _postId
-    ) public view postExists(_postId) returns (string memory) {
+    ) public view postsExists(_postId) returns (string memory) {
         return s_posts[_postId].uri;
     }
 
-    function getAllPost(
+    /// @notice Obtiene todos los posts en un rango de identificadores
+    /// @param _start El identificador inicial
+    /// @param _end El identificador final
+    /// @return Un array de posts en el rango especificado
+    function getAllPosts(
         uint256 _start,
         uint256 _end
     ) public view returns (Post[] memory) {
         uint256 length = _end - _start + 1;
-
         Post[] memory posts = new Post[](length);
-
         for (uint256 index = 0; index < length; index++) {
             posts[index] = s_posts[_start + index];
         }
-
         return posts;
     }
 
-    modifier postExists(uint256 _postId) {
+    /// @notice Modificador para verificar si un post existe
+    /// @param _postId El identificador del post
+    modifier postsExists(uint256 _postId) {
         if (_postId > s_postCounterId || _postId == 0)
             revert PostDoesNotExist(_postId);
-
         _;
     }
 }
---
+++ hardhat/docs/src/contracts/Instagram.sol/contract.Instagram.md
@@ -0,0 +1,327 @@
+# Instagram
+[Git Source](https://github.com/snrnieto/clone-instagram/blob/c1ad6dea43dac6c77b6b5b1c1a9bdd9d90c00616/contracts\Instagram.sol)
+
+**Author:**
+Sebastian Nieto
+
+You can use this contract to emulate descentralized web3 social app
+
+*A este contrato le faltan mas funcionalidades pero cumple con lo minimo*
+
+
+## State Variables
+### s_postCounterId
+Counter for posts
+
+
+```solidity
+uint256 public s_postCounterId;
+```
+
+
+### s_posts
+Mapping para almacenar los posts y su identificador
+
+
+```solidity
+mapping(uint256 => Post) private s_posts;
+```
+
+
+### s_postsUser
+Mapping para almacenar los posts de cada usuario
+
+
+```solidity
+mapping(address => mapping(uint256 => Post)) private s_postsUser;
+```
+
+
+### s_postsCounterByUser
+Conteo de posts por usuario
+
+
+```solidity
+mapping(address => uint256) private s_postsCounterByUser;
+```
+
+
+### s_likesOfPost
+Likes en las publicaciones según PostId
+
+
+```solidity
+mapping(uint256 => address[]) private s_likesOfPost;
+```
+
+
+## Functions
+### addPost
+
+Añade un nuevo post
+
+
+```solidity
+function addPost(Post memory _post) external;
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_post`|`Post`|La estructura del post que contiene la descripción y el URI|
+
+
+### like
+
+Da like a un post
+
+
+```solidity
+function like(uint256 _postId) external postsExists(_postId);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_postId`|`uint256`|El identificador del post|
+
+
+### unlike
+
+Quita el like de un post
+
+
+```solidity
+function unlike(uint256 _postId) external postsExists(_postId);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_postId`|`uint256`|El identificador del post|
+
+
+### getPost
+
+Obtiene un post por su identificador
+
+
+```solidity
+function getPost(uint256 _postId) public view postsExists(_postId) returns (Post memory);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_postId`|`uint256`|El identificador del post|
+
+**Returns**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`<none>`|`Post`|La estructura del post|
+
+
+### getPostUser
+
+Obtiene un post de un usuario por su identificador
+
+
+```solidity
+function getPostUser(address _user, uint256 _postId) public view postsExists(_postId) returns (Post memory);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_user`|`address`|La dirección del usuario|
+|`_postId`|`uint256`|El identificador del post|
+
+**Returns**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`<none>`|`Post`|La estructura del post|
+
+
+### getPostsCounterByUser
+
+Obtiene el conteo de posts de un usuario
+
+
+```solidity
+function getPostsCounterByUser(address _user) public view returns (uint256);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_user`|`address`|La dirección del usuario|
+
+**Returns**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`<none>`|`uint256`|El número de posts del usuario|
+
+
+### getLikesOfPost
+
+Obtiene el número de likes de un post
+
+
+```solidity
+function getLikesOfPost(uint256 _postId) public view postsExists(_postId) returns (uint256);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_postId`|`uint256`|El identificador del post|
+
+**Returns**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`<none>`|`uint256`|El número de likes del post|
+
+
+### getUri
+
+Obtiene el URI de un post
+
+
+```solidity
+function getUri(uint256 _postId) public view postsExists(_postId) returns (string memory);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_postId`|`uint256`|El identificador del post|
+
+**Returns**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`<none>`|`string`|El URI del post|
+
+
+### getAllPosts
+
+Obtiene todos los posts en un rango de identificadores
+
+
+```solidity
+function getAllPosts(uint256 _start, uint256 _end) public view returns (Post[] memory);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_start`|`uint256`|El identificador inicial|
+|`_end`|`uint256`|El identificador final|
+
+**Returns**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`<none>`|`Post[]`|Un array de posts en el rango especificado|
+
+
+### postsExists
+
+Modificador para verificar si un post existe
+
+
+```solidity
+modifier postsExists(uint256 _postId);
+```
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`_postId`|`uint256`|El identificador del post|
+
+
+## Events
+### PostAdded
+Evento emitido cuando se añade un nuevo post
+
+
+```solidity
+event PostAdded(uint256 indexed postId, string description, address owner);
+```
+
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`postId`|`uint256`|El identificador del post|
+|`description`|`string`|La descripción del post|
+|`owner`|`address`|La dirección del propietario del post|
+
+### Like
+Evento emitido cuando un usuario da like a un post
+
+
+```solidity
+event Like(uint256 indexed postId, address user);
+```
+
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`postId`|`uint256`|El identificador del post|
+|`user`|`address`|La dirección del usuario que dio like|
+
+### Unlike
+Evento emitido cuando un usuario quita su like de un post
+
+
+```solidity
+event Unlike(uint256 indexed postId, address user);
+```
+
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`postId`|`uint256`|El identificador del post|
+|`user`|`address`|La dirección del usuario que quitó su like|
+
+## Errors
+### PostDoesNotExist
+Error lanzado cuando un post no existe
+
+
+```solidity
+error PostDoesNotExist(uint256 postId);
+```
+
+**Parameters**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`postId`|`uint256`|El identificador del post que no existe|
+
+## Structs
+### Post
+*Un struct que define o representa un post en la plataforma*
+
+
+```solidity
+struct Post {
+    string description;
+    string uri;
+}
+```
+
+**Properties**
+
+|Name|Type|Description|
+|----|----|-----------|
+|`description`|`string`|la descripcion del post|
+|`uri`|`string`|la uri del post en IPFS|
+
